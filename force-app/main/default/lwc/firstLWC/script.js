20d60a866abc401277fcce08724ed020de633040
Kumar Garg <nikhil.kumargarg@iqvia.com>
2022-01-21 22:38:46 +0530

    JS learning Day2

--- force-app/main/default/lwc/firstLWC/script.js
+++ force-app/main/default/lwc/firstLWC/script.js
@@ -1,4 +1,5 @@
-console.log(window);
+/****Day1 Learning */
+/*console.log(window);
 var a = 10;
 console.log(window.a);
 var a = 20;
@@ -27,3 +28,45 @@ function test()
     let q = 70;
     const c = 89;
 }
+
+var a = 10.11;
+console.log(typeof a);
+var a = 12n;
+console.log(typeof a);
+var a = true;
+console.log(typeof a);
+var x;
+console.log(typeof x);
+var a = {};
+console.log(typeof a);
+var a = [];
+console.log(typeof a);
+var a = Symbol("test");
+console.log(typeof a);
+
+console.log(typeof new String("test"));
+
+var a = null;
+console.log(typeof a);
+
+console.log(3==="3");
+console.log("false"==false)
+var a;
+console.log(a);
+var b = null;
+console.log(b);
+console.log(a===b);*/
+
+var arr = [1,3,4];
+console.log(arr);
+arr.push("test");
+console.log(arr);
+
+var obj = {
+    "name":"nikhil",
+    "age":89,
+    "phone number":190099
+}
+console.log(obj);
+console.log(obj.name);
+console.log(obj["phone number"]);
